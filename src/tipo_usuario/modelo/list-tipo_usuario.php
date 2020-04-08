54255e9fe8270791d4c63859f7ab48f949557c33
rafaelcruz72 <rafael.cruz43@etec.sp.gov.br>
2020-04-08 14:56:14 -0300

    Comit do serviço list-tipo_usuario comentado

--- src/tipo_usuario/modelo/list-tipo_usuario.php
+++ src/tipo_usuario/modelo/list-tipo_usuario.php
@@ -1,36 +1,47 @@
 <?php
 
+    //Realizar o include da conexão
     include('../../conexao/conexao.php');
 
+    //Verificar se tem um conexão
     if($conexao){
 
+        //Obter o request vindo do datatable
         $requestData = $_REQUEST;
 
+        //Obter as colunas vindas do resquest
         $colunas = $requestData['columns'];
 
+        //Preparar o comando sql para obter os dados do tipo_usuario
         $sql = "SELECT idtipo_usuario, nome, tipo FROM TIPOS_USUARIOS WHERE 1=1 ";
 
+        //Obter o total de registros cadastrados
         $resultado = mysqli_query($conexao, $sql);
         $qtdeLinhas = mysqli_num_rows($resultado);
 
+        //Verificando se há filtro determinado
         $filtro = $requestData['search']['value'];
-
         if( !empty( $filtro ) ){
-
+            //Montar a expressão lógica que irá compor os filtros
+            //Aqui você deverá determinar quais colunas farão parte do filtro
             $sql .= " AND (idtipo_usuario LIKE '$filtro%' ";
             $sql .= " OR nome LIKE '$filtro%' ";
             $sql .= " OR tipo LIKE '$filtro%') ";
         }
+        //Obter o total dos dados filtrados
         $resultado = mysqli_query($conexao, $sql);
         $totalFiltrados = mysqli_num_rows($resultado);
 
-        $colunaOrdem = $requestData['order'][0]['column'];
-        $ordem = $colunas[$colunaOrdem]['data'];
-        $direcao = $requestData['order'][0]['dir'];
+        //Obter valores para ORDER BY      
+        $colunaOrdem = $requestData['order'][0]['column']; //Obtém a posição da coluna na ordenação
+        $ordem = $colunas[$colunaOrdem]['data']; //Obtém o nome da coluna para a ordenação
+        $direcao = $requestData['order'][0]['dir']; //Obtém a direção da ordenação
 
-        $inicio = $requestData['start'];
-        $tamanho = $requestData['length'];
+        //Obter valores para o LIMIT
+        $inicio = $requestData['start']; //Obtém o ínicio do limite
+        $tamanho = $requestData['length']; //Obtém o tamanho do limite
 
+        //Realizar o ORDER BY com LIMIT
         $sql .= " ORDER BY $ordem $direcao LIMIT $inicio, $tamanho ";
         $resultado = mysqli_query($conexao, $sql);
         $dados = array();
@@ -38,6 +49,7 @@
             $dados[] = array_map('utf8_encode', $row);
         }
 
+        //Monta o objeto json para retornar ao DataTable
         $json_data = array(
             "draw" => intval($requestData['draw']),
             "recordsTotal" => intval($qtdeLinhas),
@@ -45,9 +57,11 @@
             "data" => $dados
         );
 
+        //Fecha a conexão com o banco
         mysqli_close($conexao);
 
     } else{
+        //Monta um obejto json zerado para retornar ao DataTable
         $json_data = array(
             "draw" => 0,
             "recordsTotal" => 0,
@@ -56,4 +70,5 @@
         );
     }
 
+    //Retorna o objeto json para o DataTable
     echo json_encode($json_data);
