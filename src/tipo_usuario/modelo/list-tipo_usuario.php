bd1fb46356449c32f4f759917c638bf3172dd740
rafaelcruz72 <rafael.cruz43@etec.sp.gov.br>
2020-04-08 11:22:49 -0300

    Comit serviço list-tipo_usuario

---
+++ src/tipo_usuario/modelo/list-tipo_usuario.php
@@ -0,0 +1,59 @@
+<?php
+
+    include('../../conexao/conexao.php');
+
+    if($conexao){
+
+        $requestData = $_REQUEST;
+
+        $colunas = $requestData['columns'];
+
+        $sql = "SELECT idtipo_usuario, nome, tipo FROM TIPOS_USUARIOS WHERE 1=1 ";
+
+        $resultado = mysqli_query($conexao, $sql);
+        $qtdeLinhas = mysqli_num_rows($resultado);
+
+        $filtro = $requestData['search']['value'];
+
+        if( !empty( $filtro ) ){
+
+            $sql .= " AND (idtipo_usuario LIKE '$filtro%' ";
+            $sql .= " OR nome LIKE '$filtro%' ";
+            $sql .= " OR tipo LIKE '$filtro%') ";
+        }
+        $resultado = mysqli_query($conexao, $sql);
+        $totalFiltrados = mysqli_num_rows($resultado);
+
+        $colunaOrdem = $requestData['order'][0]['column'];
+        $ordem = $colunas[$colunaOrdem]['data'];
+        $direcao = $requestData['order']['dir'];
+
+        $inicio = $requestData['start'];
+        $tamanho = $requestData['length'];
+
+        $sql .= " ORDER BY $ordem $direcao LIMIT $inicio, $tamanho ";
+        $resultado = mysqli_query($conexao, $sql);
+        $dados = array();
+        while($row = mysqli_fetch_assoc($resultado)){
+            $dados[] = array_map('utf8_encode', $row);
+        }
+
+        $json_data = array(
+            "draw" => intval($requestData['draw']),
+            "recordsTotal" => intval($qtdeLinhas),
+            "recordsFiltered" => intval($totalFiltrados),
+            "data" => $dados
+        );
+
+        mysqli_close($conexao);
+
+    } else{
+        $json_data = array(
+            "draw" => 0,
+            "recordsTotal" => 0,
+            "recordsFiltered" => 0,
+            "data" => array()
+        );
+    }
+
+    echo json_encode($json_data);
