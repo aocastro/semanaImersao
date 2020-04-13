4c2ae15a760ef21f74bc4e061f0a026f32992915
aocastro <adriano.castro2@etec.sp.gov.br>
2020-04-13 09:59:58 -0300

    Commit de aprimoramentos e inclusão do view

---
+++ src/tipo_usuario/controle/view-tipo_usuario.js
@@ -0,0 +1,42 @@
+// Inicialiando o arquivo JavaScript com a leitura dos elementos do documento HTML
+$(document).ready(function() {
+    // Na linha abaixo selecionamos o elemnto do nosso HTML que tem a classe 'btn-new' e monitoramos o clique do mouse sobre ele
+    $('#table-tipo_usuario').on('click', 'button.btn-view', function(e) {
+        // A função preventDefault() trabalha atuando em cima de limpar as funcionalidades do elemento que está sendo monitorado o click
+        e.preventDefault()
+
+        alert('Clicou')
+
+        // Nesta próximas duas linhas será limpo os campos das classes selecionadas para posteiormente elas serem preenchidas de acordo com a necessidade
+        $('.modal-title').empty()
+        $('.modal-body').empty()
+
+        // Aqui iremos deternimar qual será o título que deverá aparecer no nosso modal
+        $('.modal-title').append('<h4 class="text-danger">Visualização de Tipo de Usuários</h4>')
+
+        // Aqui será inserido o formulário do arquivo que criamos anteriomente na pasta visão onde ficam nossos HTML
+        $('.modal-body').load('src/tipo_usuario/visao/form-tipo-usuario.html', function() {
+            $.ajax({
+                type: 'POST',
+                dataType: 'json',
+                url: 'src/tipo_usuario/modelo/view-tipo_usuario.php',
+                success: function(dados) {
+                    for (const dado of dados) {
+                        $('#nome').val(dados.nome)
+                        $('#nome').attr('readonly', 'true')
+                        $('#tipo').empty()
+                        $('#tipo').append(`<option value="">${dados.tipo}</option>`)
+                    }
+                }
+            })
+        })
+
+        // Uma vez que nosso Back-end aguarda os dados do formulário porém também qual o tipo de operação, iremos incluir no botão de salvar uma nova propriedade
+        // onde será incluído uma nova propiedade chamada data-operation que não tem aplicação prática em função alguma, porém será útil para
+        // demontrar qual operação desejamos realizar com os dados do formulário dentro do modal
+        // $('.btn-save').attr('data-operation', 'insert')
+
+        // Por fim iremos apresentar o modal na tela
+        $('#modal-tipo-usuario').modal('show')
+    })
+})
