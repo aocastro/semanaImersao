0d5ea5b50dc3296007c015c2186718e14ad86f4a
rafaelcruz72 <rafael.cruz43@etec.sp.gov.br>
2020-04-13 10:29:57 -0300

    Commit serviço view-tipo_usuario.php 13/04/2020

---
+++ src/tipo_usuario/modelo/view-tipo_usuario.php
@@ -0,0 +1,43 @@
+<?php
+
+    include('../../conexao/conexao.php');
+
+    $dados = array(
+        'tipo' => 'info',
+        'mensagem' => 'Não foi possível conectar ao bancoo de dados.',
+        'dados' => array()
+    );
+
+    if($conexao){
+
+        $idTipoUsuario = $_REQUEST['idtipo_usuario'];
+
+        $sql = "SELECT idtipo_usuario, nome, tipo FROM TIPOS_USUARIOS 
+                WHERE idtipo_usuario=$idTipoUsuario";
+        
+        $resultado = mysqli_query($conexao,$sql);
+
+        if($resultado){
+
+            $dadosTipo = array();
+            while($row = mysqli_fetch_assoc($resutlado)){
+                $dadosTipo = array_map('utf8_encode', $row);
+            }
+
+            $dados = array(
+                'tipo' => 'success',
+                'mensagem' => '',
+                'dados' => $dadosTipo
+            );
+        } else{
+            $dados = array(
+                'tipo' => 'error',
+                'mensagem' => 'Não foi possível obter o tipo de usuário.',
+                'dados' => array()
+            );
+        }
+
+        mysqli_close($conexao);
+    }
+
+    echo json_encode($dados);
