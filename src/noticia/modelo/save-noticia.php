b019316cfc79600959a69671eb53034e516b7879
rafaelcruz72 <rafael.cruz43@etec.sp.gov.br>
2020-04-14 11:04:18 -0300

    Commit serviço save-noticia.php - 14/04/2020

--- src/noticia/modelo/save-noticia.php
+++ src/noticia/modelo/save-noticia.php
@@ -11,14 +11,69 @@
 
             $idNoticia = isset($requestData['idnoticia']) ? $requestData['idnoticia'] : '';
             $titulo = utf8_decode($requestData['titulo']);
-            $corpo = utf8_decode($requestData['corpo']);
+            $corpo = preg_replace("/(\\r)?\\n/i", "<br/>", $resquestData['corpo']);
             $dataNoticia = str_replace('/','-',$requestData['data_noticia']);
             $dataNoticia = date('Y-m-d H:i:s', strtotime($dataNoticia));
             $idCategoria = $requestData['idcategoria'];
 
             if($requestData['operacao'] == 'insert'){
 
-               
+               //Realizar o INSERT/UPLOAD e INSERT
+               if(!empty($imagem['name']) && $imagem['error'] == 0){
+
+                    $pasta = 'img/';
+                    if(!file_exists($pasta)) mkdir($pasta, 0755);
+
+                    $arquivoTemporario = $imagem['tmp_name'];
+                    $nomeArquivo = $imagem['name'];
+                    $extensao = pathinfo($nomeArquivo, PATHINFO_EXTENSION);
+                    $extensao = strtolower($extensao);
+                    $tipoArquivo = array('jpg', 'jpeg', 'png', 'gif');
+                    if(in_array($extensao, $tipoArquivo)){
+                        $novoNome = uniqid(time()) . '.' . $extensao;
+                        $destino = $pasta . $novoNome;
+                        if(move_uploaded_file($arquivoTemporario, $destino)){
+                            $sql = "INSERT INTO NOTICIAS(data_noticia, titulo, corpo, imagem, idcategoria) VALUES('$dataNoticia', '$titulo', '$corpo', '$destino', '$idCategoria')";
+                            $resultado = mysqli_query($conexao, $sql);
+                            if($resultado){
+                                $dados = array(
+                                    'tipo' => 'success',
+                                    'mensagem' => 'Notícia salva com sucesso.'
+                                );
+                            } else{
+                                $dados = array(
+                                    'tipo' => 'error',
+                                    'mensagem' => 'Não possível salvar a notícia.'
+                                );
+                                unlink($destino);
+                            }
+                        } else{
+                            $dados = array(
+                                'tipo' => 'error',
+                                'mensagem' => 'Não possível realizar o upload.'
+                            );
+                        }
+                    }else{
+                        $dados = array(
+                            'tipo' => 'info',
+                            'mensagem' => 'Você somente poderá enviar arquivos "*.jpg; *.jpeg; *.gif; *.png"'
+                        );
+                    }
+               } else{
+                    $sql = "INSERT INTO NOTICIAS(data_noticia, titulo, corpo, idcategoria) VALUES('$dataNoticia', '$titulo', '$corpo', '$idCategoria')";
+                    $resultado = mysqli_query($conexao, $sql);
+                    if($resultado){
+                        $dados = array(
+                            'tipo' => 'success',
+                            'mensagem' => 'Notícia salva com sucesso.'
+                        );
+                    } else{
+                        $dados = array(
+                            'tipo' => 'error',
+                            'mensagem' => 'Não possível salvar a notícia.'
+                        );
+                    }
+               }
 
             } else {
 
