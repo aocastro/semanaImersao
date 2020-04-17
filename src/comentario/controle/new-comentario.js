9fffa1e22a9d4d6bd12c10b8cc5a65e818dfeba9
aocastro <adriano.castro2@etec.sp.gov.br>
2020-04-17 10:37:36 -0300

    Commit de melhorias de Front-End + comentários

---
+++ src/comentario/controle/new-comentario.js
@@ -0,0 +1,32 @@
+// Inicialiando o arquivo JavaScript com a leitura dos elementos do documento HTML
+$(document).ready(function() {
+    // Na linha abaixo selecionamos o elemnto do nosso HTML que tem a classe 'btn-new' e monitoramos o clique do mouse sobre ele
+    $('.btn-new').click(function(e) {
+        // A função preventDefault() trabalha atuando em cima de limpar as funcionalidades do elemento que está sendo monitorado o click
+        e.preventDefault()
+
+        // Nesta próximas duas linhas será limpo os campos das classes selecionadas para posteiormente elas serem preenchidas de acordo com a necessidade
+        $('.modal-title').empty()
+        $('.modal-body').empty()
+
+        // Aqui iremos deternimar qual será o título que deverá aparecer no nosso modal
+        $('.modal-title').append('<h4 class="text-danger">Novo Comentário</h4>')
+
+        // Aqui iremos criar uma constante para receber a data e hora local da criação da notícia
+        const dataComentario = new Date().toLocaleString()
+
+        // Aqui será inserido o formulário do arquivo que criamos anteriomente na pasta visão onde ficam nossos HTML
+        $('.modal-body').load('src/comentario/visao/form-comentario.html', function() {
+            $('#data_comentario').val(dataComentario)
+            $('#data_comentario').attr('readonly', 'true')
+        })
+
+        // Uma vez que nosso Back-end aguarda os dados do formulário porém também qual o tipo de operação, iremos incluir no botão de salvar uma nova propriedade
+        // onde será incluído uma nova propiedade chamada data-operation que não tem aplicação prática em função alguma, porém será útil para
+        // demontrar qual operação desejamos realizar com os dados do formulário dentro do modal
+        $('.btn-save').attr('data-operation', 'insert')
+
+        // Por fim iremos apresentar o modal na tela
+        $('#modal-comentario').modal('show')
+    })
+})
--- src/noticia/controle/all-noticia.js
+++ src/noticia/controle/all-noticia.js
@@ -7,17 +7,17 @@ $(document).ready(function() {
         url: 'src/noticia/modelo/all-noticia.php',
         success: function(dados) {
             for (const dado of dados) {
-                $('#all-noticia').after(`
+                $('#all-noticia').append(`
                     <h5 class="border-top mt-2">
                     ${dado.data_noticia} - ${dado.titulo}
                     </h5>
                     <p class="text-justify">
-                        ${dado.corpo.substring(0, 255)}
-                        <button id="${dado.idnoticia}" class="btn btn-primary btn-sm btn-view"><i class="mdi mdi-plus-circle"></i> Ler mais [${dado.idnoticia}]</button>
+                        ${dado.corpo.substring(0, 255)} 
+                        <button id="${dado.idnoticia}" class="btn btn-primary btn-sm btn-view"><i class="mdi mdi-plus-circle"></i> Ler mais</button>
                     </p>
                 `)
             }
-            // $('body').append(`<script src="src/noticia/controle/view-external-noticia.js"></script>`)
+            $('body').append(`<script src="src/noticia/controle/view-external-noticia.js"></script>`)
         }
     })
 })
